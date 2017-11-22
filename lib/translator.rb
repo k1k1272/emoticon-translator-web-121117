cec47b104c18564afa08ccf45ca946bca24b1aec
Kenny Hom <kenny.hom27@gmail.com>
2017-11-22 00:45:02 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,16 +12,10 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path)
-  emoticons.each do |k, v|
-    get_emoticon[v] = k
-    get_meaning[v] = k
-  end
+
 end
 
 def get_english_meaning(path, emoticon)
   load_library(path)
-  emoticons.each do |k, v|
-    get_emoticon[v] = k
-    get_meaning[v] = k
-  end
+
 end
