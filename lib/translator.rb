e4ead5c6578a7e079a1e32da1e1861ca0b4d011a
Kenny Hom <kenny.hom27@gmail.com>
2017-11-22 00:47:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,12 +2,6 @@ require "yaml"
 
 def load_library(path)
   emoticons = YAML.load_file(path)
-  get_emoticon = {}
-  get_meaning = {}
-  emoticons.each do |k, v|
-    get_emoticon[v[0]] = v[1]
-    get_meaning[v[1]] = v[0]
-  end
 end
 
 def get_japanese_emoticon(path, emoticon)
